5e17eff85957bbe15d18750dedf05a7b723eeb75
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-04-13 19:05:43 -0400

    Creating users is now possible
    Creating user functionality is added. userExists helper method added. userExists helper method simplifies the loadUser method.

--- core/src/com/ninetyninenercentcasino/game/Database.java
+++ core/src/com/ninetyninenercentcasino/game/Database.java
@@ -3,6 +3,7 @@ import java.sql.*;
 import java.util.Arrays;
 import java.security.*;
 import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
 
 /*
  * Class: Database
@@ -13,7 +14,7 @@ public class Database {
 
     /*
      * Name: Database
-     * Purpose: Constructor. Initializes the database connection.
+     * Description: Constructor. Initializes the database connection.
      * Precondition: None
      * Postcondition: Connection to the database established
     */
@@ -22,45 +23,62 @@ public class Database {
     }
     /*
      * Name: loadUser
-     * Purpose: Takes a username and password. Checks if the username coresponds to a real account. Checks if the password supplied is the correct password for that account. If the account exists and the password is correct, it returns a Account object.
+     * Description: Takes a username and password. Checks if the username coresponds to a real account. Checks if the password supplied is the correct password for that account. If the account exists and the password is correct, it returns a Account object.
      * Precondition: None
      * Postcondition: None
      */
     public Account loadUser(String username, String password) throws SQLException, AccountNonExistent, PasswordIncorrect {
         PreparedStatement loadUser=database.prepareStatement("select 'Username' from Accounts");
         ResultSet result=loadUser.executeQuery();
-        boolean foundUser=false;
-        while(result.next()) { //Next returns false if there is not another value.
-            if(result.getString("Username").equals(username)) {
-                foundUser=true;
-                break;
-            }
-        }
-        if(foundUser==false) {
-            throw new AccountNonExistent();
-        } else {
-            if(Arrays.equals(result.getBytes("Hash"), hash(password, result.getBytes("Salt")))) {
+        if(userExists(result, username)) { //User does exist
+            if(Arrays.equals(result.getBytes("Hash"), hash(password, result.getBytes("Salt")))) { //Compares the password provided to the password in the database
                 Account user=new Account(result.getString("Username"));
                 return user;
             } else {
-                throw new PasswordIncorrect();
+                throw new PasswordIncorrect(); //Password wrong
             }
+        } else { //User does not exist
+            throw new AccountNonExistent(); //Account does not exist
         }
     }
     /*
+     * 
+     */
+    public void createUser(String username, String password) throws SQLException {
+        SecureRandom secureRandom=new SecureRandom();
+        byte[] salt=new byte[32];
+        secureRandom.nextBytes(salt);
+        PreparedStatement createUser=database.prepareStatement("insert into Accounts ('Username', 'Hash', 'Salt') values ("+username+","+hash(password, salt)+","+salt+");");
+        createUser.executeQuery();
+    }
+    /*
      * Name: hash
-     * Purpose: Computes the hash. For use in verify user passwords. Hash algorithim is SHA-256.
+     * Description: Computes the hash. For use in verify user passwords. Hash algorithim is SHA-256.
      * Preconditon: None
      * Postcondition: The SHA-256 hash of the password with the salt appended to the end is returned as a byte array.
      */
     private byte[] hash(String password, byte[] salt) {
         byte[] hashedValue=null;
         try {
-            MessageDigest digest=MessageDigest.getInstance("SHA-256");
-            hashedValue=digest.digest(ByteArray.merge(password.getBytes(StandardCharsets.UTF_8), salt));
+            MessageDigest digest=MessageDigest.getInstance("SHA-256"); //SHA-256
+            hashedValue=digest.digest(ByteArray.merge(password.getBytes(StandardCharsets.UTF_8), salt)); //Hashes the password+salt
         } catch(Exception e) {
             System.out.println(e);
         }
         return hashedValue;
+    }
+    /*
+     * Name: userExists
+     * Description: Checks if the username exists in the database.
+     * Precondition: None
+     * Postcondition: True returned if username is in database, false if not.
+     */
+    private boolean userExists(ResultSet result, String username) throws SQLException {
+        while(result.next()) { //Next returns false if there is not another value.
+            if(result.getString("Username").equals(username)) {
+                return true; //Username exists
+            }
+        }
+        return false; //Username does not exist
     }
 }
