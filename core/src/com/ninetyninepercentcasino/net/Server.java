1d8aa54c3a6f26a4ec2b3e9a631547aabde3706e
MrMan314 <mrman314dev@gmail.com>
2024-04-29 10:50:17 -0400

    try to add keepalive

--- core/src/com/ninetyninepercentcasino/net/Server.java
+++ core/src/com/ninetyninepercentcasino/net/Server.java
@@ -4,19 +4,29 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Server extends Thread {
 	private ServerSocket serverSocket;
+	private int port = 9925;
 
+	private ArrayList<ServerThread> clients = new ArrayList<ServerThread>();
+
+	public Server() throws IOException {
+		serverSocket = new ServerSocket(this.port);
+	}
+	
 	public Server(int port) throws IOException {
-		serverSocket = new ServerSocket(port);
+		this.port = port;
+		serverSocket = new ServerSocket(this.port);
 	}
 
 	public void run() {
 		boolean running = true;
 		while (running) {
 			try {
-				new ServerThread(serverSocket.accept()).start();
+				clients.add(new ServerThread(serverSocket.accept()));
+				clients.get(clients.size() - 1).start();
 			} catch (IOException e) {
 				System.out.println(e);
 			}
@@ -40,17 +50,36 @@ public class Server extends Thread {
 			in.close();
 			out.close();
 			clientSocket.close();
+			System.out.printf("Connection closed from %s\n", clientSocket.getRemoteSocketAddress().toString());
+		}
+
+		public Socket getClientSocket() {
+			return clientSocket;
 		}
 
 		public void run() {
 			try {
 				out = new PrintWriter(clientSocket.getOutputStream(), true);
 				in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-				out.println("when the " + in.readLine());
+				System.out.printf("New connection from %s\n", clientSocket.getRemoteSocketAddress().toString());
+				while (true) {
+					try {
+        		        wait(5);
+						out.println("hello vro1!!!");
+		            } catch (InterruptedException e) {
+						System.out.println("interrupted!!!11");
+						break;
+					}
+				}
 				finish();
 			} catch (IOException e) {
 				System.out.println(e);
 			}
 		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		Server server = new Server();
+		server.start();
 	}
 }
