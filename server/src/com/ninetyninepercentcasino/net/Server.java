c1db0281f3022a1463cc3f48113c5794f48115db
MrMan314 <44348373+MrMan314@users.noreply.github.com>
2024-05-12 15:50:41 -0400

    Client and ServerThread are a bit too similar, in fact, 𝓯𝓻𝓮𝓪𝓴𝓲𝓵𝔂 similar

--- server/src/com/ninetyninepercentcasino/net/Server.java
+++ server/src/com/ninetyninepercentcasino/net/Server.java
@@ -1,21 +1,9 @@
 package com.ninetyninepercentcasino.net;
 
-import java.net.Socket;
 import java.net.ServerSocket;
-import java.net.SocketException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.IOException;
-import java.io.EOFException;
-import java.io.StreamCorruptedException;
-import java.io.OptionalDataException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.lang.IllegalStateException;
-import java.lang.IllegalMonitorStateException;
-import java.lang.NullPointerException;
 
 import com.ninetyninepercentcasino.net.Connection;
 import com.ninetyninepercentcasino.net.NetMessage;
@@ -24,7 +12,7 @@ public class Server extends Thread {
 	private ServerSocket serverSocket;
 	private int port = 9925;
 
-	private List<ServerThread> clients = new ArrayList<ServerThread>();
+	private List<Connection> clients = new ArrayList<Connection>();
 
 	public Server() throws IOException {
 		serverSocket = new ServerSocket(this.port);
@@ -39,7 +27,7 @@ public class Server extends Thread {
 		boolean running = true;
 		while (running) {
 			try {
-				clients.add(new ServerThread(serverSocket.accept()));
+				clients.add(new Connection(serverSocket.accept(), clients));
 				clients.get(clients.size() - 1).start();
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -52,54 +40,19 @@ public class Server extends Thread {
 	}
 
 	public void sendAll(NetMessage message) throws IOException {
-		for (ServerThread client: clients) {
+		for (Connection client: clients) {
 			client.message(message);
 		}
 	}
 	
-	public void sendAll(NetMessage message, ServerThread origin) throws IOException {
-		for (ServerThread client: clients) {
+	public void sendAll(NetMessage message, Connection origin) throws IOException {
+		for (Connection client: clients) {
 			if (client != origin) {
 				client.message(message);
 			}
 		}
 	}
-
-	private class ServerThread extends Connection {
-		public ServerThread(Socket socket) throws IOException {
-			try {
-				super.clientSocket = socket;
-				super.alive = true;
-				super.in = new ObjectInputStream(clientSocket.getInputStream());
-				super.out = new ObjectOutputStream(clientSocket.getOutputStream());
-				System.out.printf("New connection from %s\n", clientSocket.getRemoteSocketAddress().toString());
-				super.timer.scheduleAtFixedRate(super.keepAliveTimeout, 5000, 5000);
-			} catch (StreamCorruptedException e) {
-				this.finish();
-			}	
-		}
-
-		public void finish() throws IOException {
-			super.alive = false;
-			try {
-				super.timer.cancel();
-				super.timer.purge();
-			} catch (IllegalStateException e) {
-
-			}
-			super.clientSocket.close();
-			Thread.currentThread().interrupt();
-			try {
-				super.in.close();
-			} catch (NullPointerException e) {
-
-			}
-			super.out.close();
-			clients.remove(this);
-			System.out.printf("Connection closed from %s\n", clientSocket.getRemoteSocketAddress().toString());
-		}
-	}
-
+	
 	public static void main(String[] args) throws IOException {
 		Server server = new Server();
 		server.start();
