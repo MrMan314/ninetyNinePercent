5737f0ad46cf7a5cb64cbe1b6204758c408974ae
MrMan314 <mrman314dev@gmail.com>
2024-05-30 10:20:37 -0400

    add comments that i forgor to commit and accidentally removed

--- server/src/com/ninetyninepercentcasino/net/ServerConnection.java
+++ server/src/com/ninetyninepercentcasino/net/ServerConnection.java
@@ -1,3 +1,8 @@
+/**
+ * ServerConnection.java
+ * ServerConnection object, child of Connection
+ */
+
 package com.ninetyninepercentcasino.net;
 
 import com.ninetyninepercentcasino.database.Account;
@@ -10,35 +15,54 @@ import java.util.List;
 
 public class ServerConnection extends Connection {
     private BJGame bjGame;
-    public ServerConnection(Socket clientSocket) throws IOException {
-        super(clientSocket);
-    }
+
+	/**
+	 * Constructor for ServerConnection, calls parent constructor
+	 * pre: clientSocket and list of clients exists
+	 * post: ServerConnection and Connection is started
+	 */
     public ServerConnection(Socket clientSocket, List<Connection> clients) throws IOException {
         super(clientSocket, clients);
     }
+
+	/**
+	 * Main method for ServerConnection thread
+	 * pre: ServerConnection is started
+	 * post: ServerConnection is run
+	 */
     @Override
     public void run(){
         try {
+			// Loop until dead
             while (alive) {
+				// If connection is closed, quit
                 if(!clientSocket.isConnected()) {
                     finish();
                 }
                 try {
-                    NetMessage message = (NetMessage) in.readObject();
-                    message.setOrigin(clientSocket.getRemoteSocketAddress());
-                    if (message.getContent() != null) {
-                        System.out.printf("[%s] %s: %s\n",  message.getType(), clientSocket.getRemoteSocketAddress().toString(), message.getContent());
-                        switch(message.getType()) {
-                            case ACK:
-                                aliveMessage = (String) message.getContent();
-                                break;
-                            case PING:
-                                message.setType(NetMessage.MessageType.ACK);
-                                out.writeObject(message);
+					// Read message from input stream
+					NetMessage message = (NetMessage) in.readObject();
+					// Set the origin of the message on the server side
+					message.setOrigin(clientSocket.getRemoteSocketAddress());
+					// Run if the message is not bogus
+					if (message.getContent() != null) {
+						// Log the message to the console
+						System.out.printf("[%s] %s: %s\n",  message.getType(), clientSocket.getRemoteSocketAddress().toString(), message.getContent());
+						switch(message.getType()) {						
+							case ACK:
+								// Set the aliveMessage to the content of the message
+								aliveMessage = (String) message.getContent();
+								break;
+							case PING:
+								// Set the type of the message to ACK and send it back
+								message.setType(NetMessage.MessageType.ACK);
+								out.writeObject(message);
                                 break;
                             case INFO:
+								// Read content of message
                                 Object content = message.getContent();
                                 if(content instanceof String){
+									// Start game if request says so, inform user as well
                                     if(message.getContent().equals("begin game.")){
                                         bjGame = new BJGame(new BJPlayer(new Account("REPLACE"), this));
                                         bjGame.start();
@@ -46,6 +70,7 @@ public class ServerConnection extends Connection {
                                     }
                                 }
                                 if(content instanceof BJBetRequest) {
+									// Process BJBetRequest if it is a BJBetRequest
                                     bjGame.setFirstBet(((BJBetRequest)content).getAmountBet());
                                     synchronized(bjGame.getBjSynchronizer()) {
                                         bjGame.getBjSynchronizer().notify();
@@ -53,6 +78,7 @@ public class ServerConnection extends Connection {
                                     System.out.println("BET PLACED: " + ((BJBetRequest)content).getAmountBet());
                                 }
                                 else if(content instanceof BJActionUpdate){
+									// Update action, notify synchronizer
                                     bjGame.setAction(((BJActionUpdate)content).getChosenAction());
                                     synchronized(bjGame.getBjSynchronizer()) {
                                         bjGame.getBjSynchronizer().notify();
@@ -61,13 +87,15 @@ public class ServerConnection extends Connection {
                             default:
                         }
                     }
-                } catch (OptionalDataException e) {
-
-                } catch (EOFException e) {
-                    finish();
-                } catch (IOException | ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
+				} catch (OptionalDataException e) {
+					// This error can be safely ignored.
+				} catch (EOFException e) {
+					// This occurs as a result of the 
+					finish();
+				} catch (IOException | ClassNotFoundException e) {
+					// These errors cannot be ignored
+					e.printStackTrace();
+				}
             }
         } catch (IOException e) {
             e.printStackTrace();
