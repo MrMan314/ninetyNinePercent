56ddd022ff3c8e3de2b065e2a4b0489911bc1b0d
MrMan314 <44348373+MrMan314@users.noreply.github.com>
2024-06-01 14:33:27 -0400

    who reverted the server comments :(((

--- server/src/com/ninetyninepercentcasino/net/Server.java
+++ server/src/com/ninetyninepercentcasino/net/Server.java
@@ -1,3 +1,8 @@
+/**
+ * Server.java
+ * Server object to accept connections from clients
+ */
+
 package com.ninetyninepercentcasino.net;
 
 import com.ninetyninepercentcasino.database.Database;
@@ -9,12 +14,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Server extends Thread {
+	// Server socket and port
 	private ServerSocket serverSocket;
 	private int port = 9925;
 	private Database database;
 
+	// List of connections
 	private List<Connection> clients = new ArrayList<Connection>();
 
+	/**
+	 * Constructor for Server without port
+	 * pre: none
+	 * post: Server started on port 9925 (default)
+	 */
 	public Server() throws IOException {
 		serverSocket = new ServerSocket(this.port);
 		try { 
@@ -24,6 +36,11 @@ public class Server extends Thread {
 		}
 	}
 
+	/**
+	 * Constructor for Server with port
+	 * pre: none
+	 * post: Server started on the specified ports
+	 */
 	public Server(int port) throws IOException {
 		this.port = port;
 		serverSocket = new ServerSocket(this.port);
@@ -34,38 +51,57 @@ public class Server extends Thread {
 		}
 	}
 
+	/**
+	 * Main method of the server to accept connections
+	 * pre: Server object is initialized
+	 * post: Server has been run
+	 */
 	public void run() {
+		// Running flag
 		boolean running = true;
 		while (running) {
 			try {
+				// Accept a new client from the next client connected
 				clients.add(new ServerConnection(serverSocket.accept(), clients, database));
+				// Start the new client connection thread
 				clients.get(clients.size() - 1).start();
 			} catch (IOException e) {
+				// Show erro
 				e.printStackTrace();
 			}
 		}
 	}
 
+	/**
+	 * Method to stop the server
+	 * pre: Server is started
+	 * post: Server is stopped
+	 */
 	public void finish() throws IOException {
 		serverSocket.close();
 	}
 
+	/**
+	 * Method to send a message to all client connection threads
+	 * pre: Server is started
+	 * post: message is sent to all connected and alive clients
+	 */
 	public void sendAll(NetMessage message) throws IOException {
 		for (Connection client: clients) {
 			client.message(message);
 		}
 	}
-	
+
+	/**
+	 * Method to send a message to all client connection threads, with an origin
+	 * pre: Server is started
+	 * post: message is sent to all connected and alive clients
+	 */
 	public void sendAll(NetMessage message, Connection origin) throws IOException {
 		for (Connection client: clients) {
 			if (client != origin) {
 				client.message(message);
 			}
 		}
-	}
-	
-	public static void main(String[] args) throws IOException {
-		Server server = new Server();
-		server.start();
 	}
 }
