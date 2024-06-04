5622aa8c0ccb1d4e7b99c3c30af578b38a8458f6
MrMan314 <mrman314dev@gmail.com>
2024-06-04 10:03:00 -0400

    add holepunchconnection prelimenary code

---
+++ desktop/src/com/ninetyninepercentcasino/net/HolePunchConection.java
@@ -0,0 +1,7 @@
+package com.ninetyninepercentcasino.net;
+
+public class HolePunchConnection extends Connection {
+	public HolePunchConnection(Socket clientSocket) throws IOException {
+		super(clientSocket);
+	}
+}
