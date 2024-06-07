7592d3db09dc6d3442864a5f0147c8f217db0069
MrMan314 <mrman314dev@gmail.com>
2024-06-07 10:50:39 -0400

    more holepunch testing

--- desktop/src/com/ninetyninepercentcasino/net/HolePunchConnection.java
+++ desktop/src/com/ninetyninepercentcasino/net/HolePunchConnection.java
@@ -1,53 +1,107 @@
 package com.ninetyninepercentcasino.net;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OptionalDataException;
 import java.io.EOFException;
 import java.io.IOException;
+import java.io.StreamCorruptedException;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.net.ServerSocket;
 
 public class HolePunchConnection extends Connection {
 	private Socket punchSocket;
 	private ServerSocket punchListener;
+	private SocketAddress externalAddress;
+
+	private ObjectInputStream punchIn;
+	private ObjectOutputStream punchOut;
 
 	public HolePunchConnection(Socket clientSocket, Socket punchSocket) throws IOException {
 		super(clientSocket);
+		try {
+			// Initialize the input and output streams, should be switched around from the order of the server
+			punchIn = new ObjectInputStream(clientSocket.getInputStream());
+			punchOut = new ObjectOutputStream(clientSocket.getOutputStream());
+		} catch (StreamCorruptedException e) {
+			// In case of a corrupt connection, exit
+			finish();
+		}
 		this.punchSocket = punchSocket;
 	}
 
 	public void run() {
-		try {
-			while (alive) {
-				if(!clientSocket.isConnected()) {
-					finish();
-				}
+		new Thread() {
+			public void run() {
 				try {
-					NetMessage message = (NetMessage) in.readObject();
-					message.setOrigin(clientSocket.getRemoteSocketAddress());
-					if (message.getContent() != null) {
-						System.out.printf("[%s] %s: %s\n",  message.getType(), clientSocket.getRemoteSocketAddress().toString(), message.getContent());
-						switch(message.getType()) {
-							case ACK:
-								aliveMessage = (String) message.getContent();
-								break;
-							case PING:
-								message.setType(NetMessage.MessageType.ACK);
-								out.writeObject(message);
-								break;
-							default:
+					while (alive) {
+						if(!clientSocket.isConnected()) {
+							finish();
+						}
+						try {
+							NetMessage message = (NetMessage) in.readObject();
+							message.setOrigin(clientSocket.getRemoteSocketAddress());
+							if (message.getContent() != null) {
+								System.out.printf("[%s] %s: %s\n",  message.getType(), clientSocket.getRemoteSocketAddress().toString(), message.getContent());
+								switch(message.getType()) {
+									case ACK:
+										aliveMessage = (String) message.getContent();
+										break;
+									case PING:
+										message.setType(NetMessage.MessageType.ACK);
+										out.writeObject(message);
+										break;
+									case INFO:
+										if (message.getContent() instanceof SocketAddress) {
+											synchronized (externalAddress) {
+												externalAddress = (SocketAddress) message.getContent();
+												externalAddress.notify();
+											}
+										}
+									case NORMAL:
+										if (message.getContent() instanceof SocketAddress) {
+											synchronized (externalAddress) {
+												externalAddress = (SocketAddress) message.getContent();
+												externalAddress.notify();
+											}
+										}
+									default:
+								}
+							}
+						} catch (OptionalDataException e) {
+
+						} catch (EOFException e) {
+							finish();
+						} catch (IOException | ClassNotFoundException e) {
+							e.printStackTrace();
 						}
 					}
-				} catch (OptionalDataException e) {
-
-				} catch (EOFException e) {
-					finish();
-				} catch (IOException | ClassNotFoundException e) {
+				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		}.start();
+		new Thread() {
+			public void run() {
+				try {
+					synchronized (externalAddress) {
+						externalAddress.wait();
+					}
+					synchronized (punchOut) {
+						NetMessage message = new NetMessage(NetMessage.MessageType.INFO, externalAddress);
+						punchOut.writeObject(message);
+					}
+					synchronized (externalAddress) {
+						externalAddress.wait();
+					}
+					System.out.println(externalAddress);
+	//				punchListener = new ServerSocket(externalAddress.);
+				} catch (IOException | InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}.start();	
 	}
 
 	public static void main(String[] args) throws IOException {
