ac50ab91244f7598a3664d626b97de235ca38a17
MrMan314 <44348373+MrMan314@users.noreply.github.com>
2024-06-08 15:52:54 -0400

    no more broker for now

--- server/src/com/ninetyninepercentcasino/ServerLauncher.java
+++ server/src/com/ninetyninepercentcasino/ServerLauncher.java
@@ -8,8 +8,6 @@ import java.io.IOException;
 public class ServerLauncher {
 	public static void main(String[] args) throws IOException {
 		BJServer server = new BJServer();
-		Broker broker = new Broker();
 		server.start();
-		broker.start();
 	}
 }
